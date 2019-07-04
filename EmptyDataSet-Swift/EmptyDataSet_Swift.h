2855cc35551290657f2377c66bf15280cd0b69b7
Mateusz Wagner <mateusz.wagner@appunite.com>
2019-07-04 13:00:29 +0100

    Adds framework target

---
+++ EmptyDataSet-Swift/EmptyDataSet_Swift.h
@@ -0,0 +1,19 @@
+//
+//  EmptyDataSet_Swift.h
+//  EmptyDataSet-Swift
+//
+//  Created by Mateusz Wagner on 04/07/2019.
+//  Copyright © 2019 Xiaoye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EmptyDataSet_Swift.
+FOUNDATION_EXPORT double EmptyDataSet_SwiftVersionNumber;
+
+//! Project version string for EmptyDataSet_Swift.
+FOUNDATION_EXPORT const unsigned char EmptyDataSet_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EmptyDataSet_Swift/PublicHeader.h>
+
+
